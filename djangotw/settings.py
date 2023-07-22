86c5e3cf5e0fea9e5716e2ca34df8a9f8166bb44
Andrew.xyz <andrew@tenty.xyz>
2023-07-22 10:24:46 -0500

    Allowed Hosts

--- djangotw/settings.py
+++ djangotw/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-bmtx@k4iet=gc^n9l=s&@ya!j&cjheuznse_er&6-3-znsru*r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
 INTERNAL_IPS = [ip[: ip.rfind(".")] + ".1" for ip in ips] + ["127.0.0.1", "10.0.2.2"]
