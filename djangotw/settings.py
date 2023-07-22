fe3d8b686f1736eeae2dcded78f2312749724704
Andrew.xyz <andrew@tenty.xyz>
2023-07-22 16:04:45 -0500

    Update Staticfiles Dirs

--- djangotw/settings.py
+++ djangotw/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 STATICFILES_DIRS = [
-    Path(BASE_DIR, "jstoolchain/css"),
+    Path(BASE_DIR, "jstoolchain/static"),
 ]
 
 # Quick-start development settings - unsuitable for production
