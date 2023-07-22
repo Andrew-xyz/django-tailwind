53ef601111e210b92cd9c84344bc8d91431a4c31
Andrew.xyz <andrew@tenty.xyz>
2023-07-22 09:34:57 -0500

    Move JS Toolchain out of Staticfiles

--- djangotw/settings.py
+++ djangotw/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    Path(BASE_DIR, "jstoolchain/css"),
 ]
 
 # Quick-start development settings - unsuitable for production
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = Path(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
