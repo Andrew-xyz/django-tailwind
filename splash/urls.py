be5f1c9f00a17d35fe57994d57f7fd0596f00a00
Andrew.xyz <andrew@tenty.xyz>
2023-07-30 21:20:51 -0500

    Add Success Page

--- splash/urls.py
+++ splash/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('page_one', views.page_one, name='page_one'),
     path('page_two', views.page_two, name='page_two'),
     path('page_three', views.page_three, name='page_three'),
+
+    path('page_one_success', views.page_one_success, name='page_one_success'),
 ]
